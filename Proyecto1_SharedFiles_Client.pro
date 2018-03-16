5f3b59d978e1c1875dc6945d00e267cd6cf91ab6
MacoChave <totto_cha@hotmail.com>
2018-03-15 23:15:23 -0600

    Cliente envía-recibe

---
+++ Proyecto1_SharedFiles_Client.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2018-03-15T20:41:24
+#
+#-------------------------------------------------
+
+QT       += core gui network
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Proyecto1_SharedFiles_Client
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ mainwindow.cpp
@@ -0,0 +1,37 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    tcpCliente = new QTcpSocket(this);
+    tcpCliente->connectToHost(QHostAddress::LocalHost, 1234);
+    connect(
+                tcpCliente,
+                SIGNAL (readyRead()),
+                this,
+                SLOT (leerServidor())
+            );
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::leerServidor()
+{
+    QByteArray buffer;
+    buffer.resize(tcpCliente->bytesAvailable());
+    tcpCliente->read(buffer.data(), buffer.size());
+    ui->edtLog->append((QString) buffer);
+    qDebug() << QString (buffer);
+}
+
+void MainWindow::on_btnMensaje_clicked()
+{
+    tcpCliente->write(ui->edtMensaje->text().toLatin1().data(), ui->edtMensaje->text().size());
+}
